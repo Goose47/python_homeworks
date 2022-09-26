e15ea4df01d8df4896e0b2cd72fb0aa6c8ac4a54
NikitaT <nikita.t@appomart.com>
2022-09-26 12:52:33 +0200

    added caesar encrypt function

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -15,7 +15,32 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     ''
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+
+    az_min = 97
+    az_max = 122
+    AZ_min = 65
+    AZ_max = 90
+
+    for letter in plaintext:
+        code = ord(letter) + shift
+
+        if not ((AZ_min + shift <= code <= AZ_max + shift) or (az_min + shift <= code <= az_max + shift)):
+            ciphertext += letter
+            continue
+
+        if code <= AZ_max + shift:
+            if code - AZ_max > 0:
+                next_letter = chr(AZ_min + (code - AZ_max) - 1)
+            else:
+                next_letter = chr(code)
+        else:
+            if code - az_max > 0:
+                next_letter = chr(az_min + (code - az_max) - 1)
+            else:
+                next_letter = chr(code)
+
+        ciphertext += next_letter
+
     return ciphertext
 
 
@@ -44,3 +69,8 @@ def caesar_breaker_brute_force(ciphertext: str, dictionary: tp.Set[str]) -> int:
     best_shift = 0
     # PUT YOUR CODE HERE
     return best_shift
+
+
+if __name__ == '__main__':
+    c = encrypt_caesar('PYThon', 3)
+    print(c)
