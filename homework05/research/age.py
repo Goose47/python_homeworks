0f5f315a5ccb4837ffabf0621b16da95d094ee5f
Nikita <tyumin000@gmail.com>
2022-10-14 15:35:41 +0200

    added age_pridict function

--- homework05/research/age.py
+++ homework05/research/age.py
@@ -14,4 +14,22 @@ def age_predict(user_id: int) -> tp.Optional[float]:
     :param user_id: Идентификатор пользователя.
     :return: Медианный возраст пользователя.
     """
-    pass
+    current_year = dt.date.today().year
+    friends = get_friends(user_id=user_id, fields=['bdate'])
+
+    ages = []
+
+    for friend in friends.items:
+        try:
+            date = dt.datetime.strptime(friend['bdate'], '%d.%m.%Y')
+            age = current_year - date.year
+            ages.append(age)
+        except ValueError:
+            pass
+        except KeyError:
+            pass
+
+    if len(ages) == 0:
+        return None
+
+    return statistics.mean(ages)
