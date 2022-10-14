6e87d2a80903ff9a5c88aad1de115fceadcb960f
Nikita <tyumin000@gmail.com>
2022-10-14 12:54:19 +0200

    session class added

--- homework05/vkapi/session.py
+++ homework05/vkapi/session.py
@@ -3,6 +3,9 @@ import typing as tp
 import requests
 from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.util.retry import Retry
+from requests.exceptions import ConnectionError, HTTPError, ReadTimeout, RetryError
+
+import time
 
 
 class Session:
@@ -22,10 +25,58 @@ class Session:
         max_retries: int = 3,
         backoff_factor: float = 0.3,
     ) -> None:
-        pass
+        self.base_url = base_url
+        self.timeout = timeout
+        self.max_retries = max_retries
+        self.backoff_factor = backoff_factor
 
     def get(self, url: str, *args: tp.Any, **kwargs: tp.Any) -> requests.Response:
-        pass
+        retries_count = 0
+        start_time = time.time()
+        delay = 0
+
+        while True:
+            end_time = time.time()
+            time_diff = end_time - start_time
+            # print(f'start_time: {start_time}')
+            # print(f'end_time: {end_time}')
+            # print(f'time_diff: {time_diff}')
+
+            if retries_count > self.max_retries:
+                raise RetryError
+            if time_diff >= self.timeout:
+                raise ReadTimeout
+            time.sleep(delay)
+            response = requests.get(self.base_url + url)
+
+            if response.ok:
+                return response
+            else:
+                delay = self.backoff_factor * (2 ** (retries_count - 1))
+                retries_count += 1
 
     def post(self, url: str, *args: tp.Any, **kwargs: tp.Any) -> requests.Response:
-        pass
+        retries_count = 0
+        start_time = time.time()
+        delay = 0
+        payload = kwargs['payload']
+
+        while True:
+            end_time = time.time()
+            time_diff = end_time - start_time
+            # print(f'start_time: {start_time}')
+            # print(f'end_time: {end_time}')
+            # print(f'time_diff: {time_diff}')
+
+            if retries_count > self.max_retries:
+                raise RetryError
+            if time_diff >= self.timeout:
+                raise ReadTimeout
+            time.sleep(delay)
+            response = requests.get(self.base_url + url, data=payload)
+
+            if response.ok:
+                return response
+            else:
+                delay = self.backoff_factor * (2 ** (retries_count - 1))
+                retries_count += 1
