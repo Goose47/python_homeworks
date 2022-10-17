a93bb1e6c0c532f2d2011d4b63d63d62598b6ef9
Nikita <tyumin000@gmail.com>
2022-10-17 10:33:07 +0200

    added news extraction functions

--- homework06/scraputils.py
+++ homework06/scraputils.py
@@ -6,14 +6,46 @@ def extract_news(parser):
     """ Extract news from a given web page """
     news_list = []
 
-    # PUT YOUR CODE HERE
+    table = parser.findAll('table')[2]
+    rows = table.findAll('tr')
+    i = 0
+    for row in rows:
+        tds = row.findAll('td')
+        for td in tds:
+            if not td.has_attr('class'):
+                continue
+
+            if td['class'][0] == 'title':
+                if not td.span:
+                    break
+                if td.span.a:
+                    news_list.append({})
+                    news_list[i]['title'] = td.span.a.contents[0]
+                if td.span.span and td.span.span.a:
+                    news_list[i]['url'] = 'http://' + td.span.span.a.span.contents[0]
+
+            if td['class'][0] == 'subtext':
+                if not td.span:
+                    break
+                if td.span.a:
+                    news_list[i]['author'] = td.span.a.contents[0]
+                anchor = td.span.findAll('a')[-1]
+                if anchor and not anchor.parent['class'][0] == 'age':
+                    news_list[i]['comments'] = int(anchor.contents[0].split('\xa0')[0])
+                if td.span.span:
+                    news_list[i]['points'] = int(td.span.span.contents[0].split(' ')[0])
+                    i += 1
+    del news_list[-1]
 
     return news_list
 
 
 def extract_next_page(parser):
     """ Extract next page URL """
-    # PUT YOUR CODE HERE
+    anchors = parser.findAll('a')
+    for anchor in anchors:
+        if anchor.has_attr('class') and anchor['class'][0] == 'morelink':
+            return anchor['href']
 
 
 def get_news(url, n_pages=1):
@@ -30,3 +62,6 @@ def get_news(url, n_pages=1):
         n_pages -= 1
     return news
 
+
+if __name__ == '__main__':
+    get_news('https://news.ycombinator.com', 1)
