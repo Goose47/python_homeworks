f2c6d795dcc38ff752781e75c754977d2740b9a3
Nikita <tyumin000@gmail.com>
2022-10-19 18:19:20 +0200

    added add_label and update_news functions

--- homework06/hackernews.py
+++ homework06/hackernews.py
@@ -2,7 +2,7 @@ from bottle import (
     route, run, template, request, redirect
 )
 
-from scrapper import get_news
+from scraputils import get_news
 from db import News, session
 from bayes import NaiveBayesClassifier
 
@@ -16,21 +16,49 @@ def news_list():
 
 @route("/add_label/")
 def add_label():
-    # PUT YOUR CODE HERE
+    s = session()
+    id = request.query["id"]
+    label = request.query["label"]
+
+    item = s.query(News).get(id)
+    item.label = label
+    s.commit()
+
     redirect("/news")
 
 
 @route("/update")
 def update_news():
-    # PUT YOUR CODE HERE
-    redirect("/news")
+    s = session()
+    news = get_news('https://news.ycombinator.com/newest')
+
+    for el in news:
+        title = el['title']
+        if not list(s.query(News).filter(News.title == title)):
+            title = el['title'] if 'title' in el else ''
+            author = el['author'] if 'author' in el else ''
+            comments = el['comments'] if 'comments' in el else 0
+            points = el['points'] if 'points' in el else 0
+            url = el['url'] if 'url' in el else ''
+            new_el = News(
+                title=title,
+                author=author,
+                url=url,
+                comments=comments,
+                points=points)
+            s.add(new_el)
+
+    s.commit()
+
+    redirect('/news')
 
 
 @route("/classify")
 def classify_news():
     # PUT YOUR CODE HERE
+    pass
 
 
 if __name__ == "__main__":
-    run(host="localhost", port=8080)
+    run(host="localhost", port=1234)
 
